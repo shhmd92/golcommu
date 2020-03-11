a5e53b808cb953bdf7bbf1e1ea1e2caf5e9644a8
shhmd92 <sho.hamada.92@gmail.com>
2020-03-11 22:24:52 +0900

    Userテーブルにプロフィール用のカラムを追加

---
+++ db/migrate/20200311124117_add_profile_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddProfileColumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :introduction, :text
+    add_column :users, :play_type, :integer
+    add_column :users, :prefecture, :integer
+    add_column :users, :score_average, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_055148) do
+ActiveRecord::Schema.define(version: 2020_03_11_131845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 2020_03_08_055148) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "avatar"
+    t.text "introduction"
+    t.integer "play_type"
+    t.integer "prefecture", default: 0
+    t.integer "score_average"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
