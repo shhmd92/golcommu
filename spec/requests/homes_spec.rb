ee83fdc8af3a0523f1a304f5abe7fb7cfda22a2a
shhmd92 <sho.hamada.92@gmail.com>
2020-04-25 16:50:14 +0900

    トップ画面のリクエストテストを追加

---
+++ spec/requests/homes_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Homes', type: :request do
+  describe 'GET /' do
+    example '正常にレスポンスを返すこと' do
+      get root_path
+      expect(response).to have_http_status 200
+    end
+  end
+end
