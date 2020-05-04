205192d395c59a36280419c2063a62f9ac0374f8
shhmd92 <sho.hamada.92@gmail.com>
2020-05-04 22:37:51 +0900

    ssh設定の変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ server '18.178.138.177',
   user: 'sho', 
   roles: %w{app db web},
   ssh_options: {
-    keys: ["#{ENV['PRODUCTION_SSH_KEY']}"], 
+    keys: [File.expand_path('~/.ssh/golcommu_key_rsa')],
     forward_agent: true,
     auth_methods: %w(publickey),
     port: 22
